2b6ebfa8a9bffdbf1f1b120bc4ae3d3e2cbd5a4f
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2022-01-29 23:54:30 +0530

    progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import dynamic from 'next/dynamic';
+const ProgressBar = dynamic(() => import('../components/ProgressBar'), { ssr: false });
 
 function MyApp({ Component, pageProps }) {
   return (
